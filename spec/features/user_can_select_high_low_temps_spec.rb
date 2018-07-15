37ff4db8ac0895ace108a68407758f3f7d8d5405
Emmie Hayes <emmiehayes2@gmail.com>
2018-07-15 16:25:07 -0600

    high temp low temp select testing

--- spec/features/user_can_select_high_low_temps_spec.rb
+++ spec/features/user_can_select_high_low_temps_spec.rb
@@ -4,14 +4,17 @@ describe 'Users' do
 
   it 'can select a high temp and a low temp' do
     user_1 = User.create!(name: 'Emmie Hayes', email_address: 'emmie@google.com', password_digest: 'asgaesdg')
-    user.high_pref = 70
-    user.low_pref = 60
+    location_1 = Location.create!(nick_name: 'Boulder', state: 'Colorado', high_temp: 70, low_temp: 60)
+    location_1 = Location.create!(nick_name: 'Denver', state: 'Colorado', high_temp: 100, low_temp: 90)
+    user_1.high_pref = 70
+    user_1.low_pref = 60
+
     visit user_path(user_1)
 
-    select user.high_pref, from: :high_temperature
-    select user.low_pref, from: :low_temperature
+    select user_1.high_pref, from: :high_temperature
+    select user_1.low_pref, from: :low_temperature
 
-    expect(current_path).to eq(user_path(1))
-    expect(page).to have_content("Denver")
-    expect(page).to_not have_content("Boulder")
+    expect(current_path).to eq(user_path(user_1))
+    expect(page).to have_content("Boulder")
+    expect(page).to_not have_content("Denver")
   end
