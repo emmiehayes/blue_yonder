2beeddc3784d1b2bb801ca458da5b3bc88be90a9
Emmie Hayes <emmiehayes2@gmail.com>
2018-07-17 11:07:36 -0600

    add test for admin delete user functionality

--- spec/features/admin_sees_dashboard_index_page_spec.rb
+++ spec/features/admin_sees_dashboard_index_page_spec.rb
@@ -24,4 +24,19 @@ describe 'user visits dashboard index page' do
       expect(page).to have_content('The page you were looking for doesn\'t exist')
     end
   end
+
+  context 'as an admin user' do
+    it 'can delete a default user' do
+      admin = User.create!(name: 'Emmie', email_address: 'emmie@gmail.com', password: 'password', role: 1)
+      user = User.create!(name: 'Bob', email_address: 'bob@gmail.com', password: 'password')
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+      visit admin_dashboard_index_path
+      click_link 'Delete User'
+
+      expect(page).to_not have_content('Bob')
+      expect(page).to_not have_content('bob@gmail.com')
+    end
+  end
 end
