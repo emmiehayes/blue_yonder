2cc2a12ce572b207c6315dc137a6568bb0815774
Emmie Hayes <emmiehayes2@gmail.com>
2018-07-17 10:49:45 -0600

    add model spec for user roles

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,4 +14,19 @@ describe User, type: :model do
     it {should have_many(:favorites)}
     it {should have_many(:locations).through(:favorites)}
   end
+
+  describe 'roles' do
+    it 'can be created as an admin' do
+      user = User.create(user_name: 'Emmie', password: 'password', role: 1)
+      expect(user.role).to eq('admin')
+      expect(user.admin?).to be_truthy
+    end
+
+    it 'can be created as a default user' do
+      user = User.create(user_name: 'Bob', password: 'test123', role: 0)
+
+      expect(user.role).to eq('default')
+      expect(user.default?).to be_truthy
+    end
+  end
 end
