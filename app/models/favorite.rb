40a5a6a6de453a9abf86b38610829c5fc7b00347
Emmie Hayes <emmiehayes2@gmail.com>
2018-07-18 13:12:11 -0600

    add class methods and pass test for favorite

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,16 @@
 class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :location
+
+  def self.total_locations_favorited
+    distinct.pluck(:location_id).count
+  end
+
+  def self.total_users_with_a_favorite
+    distinct.pluck(:user_id).count
+  end
+
+  def self.most_favorited
+    group(:location_id).count
+  end
 end
