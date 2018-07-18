daf024342ffe61fc67eb882d9f7b0b7c34a03458
Emmie Hayes <emmiehayes2@gmail.com>
2018-07-17 19:00:52 -0600

    planning for favorites approach and expanding seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-LOCATION_LIST = [["Boulder", "Colorado", 60, 70],["Denver", "Colorado", 90, 100],["Steamboat Springs", "Colorado", 66, 89],["Golden", "Colorado", 95, 90], ["Salida", "Colorado", 65, 78],["Breckenridge", "Colorado", 60, 75]]
+admin = User.create!(name: 'Emmie', email_address: 'emmiehayes2@gmail.com', password: 'password', role: 1)
+LOCATION_LIST = [
+                ["Boulder", "Colorado", 10, 20],
+                ["Denver", "Colorado", 20, 30],
+                ["Steamboat Springs", "Colorado", 40, 50],
+                ["Golden", "Colorado", 60, 70],
+                ["Salida", "Colorado", 80, 90],
+                ["Vail", "Colorado", 90, 100],
+                ["Breckenridge", "Colorado", 65, 75],
+                ["Telluride", "Colorado", 55, 95],
+                ["Fort Collins", "Colorado", 74, 99],
+                ["Colorado Springs", "Colorado", 75, 80],
+                ["Winter Park", "Colorado", 75, 80],
+                ["Crested Butte", "Colorado", 75, 80],
+                ["Fruita", "Colorado", 75, 80],
+                ["Estes Park", "Colorado", 75, 80],
+                ["Durango", "Colorado", 75, 80],
+                ["Canon City", "Colorado", 75, 80],
+                ["Aspen", "Colorado", 75, 80]
+                ]
 
 LOCATION_LIST.each do |nick_name, state, low, high|
   Location.create( nick_name: nick_name, state: state, low_temp: low, high_temp: high)
 end
-
-admin = User.create!(name: 'Emmie', email_address: 'emmiehayes2@gmail.com', password: 'password', role: 1)
