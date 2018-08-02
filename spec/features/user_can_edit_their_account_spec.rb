48e76c2b2754645fe0879ff48816a68469fe9445
Emmie Hayes <emmiehayes2@gmail.com>
2018-08-02 11:51:30 -0600

    edit user info and create account specs now passing

--- spec/features/user_can_edit_their_account_spec.rb
+++ spec/features/user_can_edit_their_account_spec.rb
@@ -4,10 +4,10 @@ describe 'edit account workflow' do
 
   it 'submits edit form successfully' do
     user = User.create!(name: 'Emmie', email_address: 'emmie@google.com', password: 'asgaesdg', high_pref: 0, low_pref: 0)
-    new_name = 'Change'
+    new_name = 'Emily'
 
     visit user_path(user)
-    
+
     click_on 'Edit Account'
 
     expect(current_path).to eq(edit_user_path(user))
@@ -16,7 +16,7 @@ describe 'edit account workflow' do
     click_button 'Update'
 
     expect(current_path).to eq(user_path(user))
-    expect(page).to have_content("Change's Favorites")
+    expect(page).to have_content("Emily's Favorite Spots")
     expect(page).to_not have_content('Emmie')
   end
 end
